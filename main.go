9d3384ec7f8026003774292a2d827180c0ab6105
Pablo Sousa <ph.info.cont@rede.ulbra.br>
2024-11-03 15:58:56 -0300

    feat: demo route groups

--- main.go
+++ main.go
@@ -12,11 +12,19 @@ var f embed.FS
 
 func main() {
 	router := gin.Default()
-	router.GET("/employees/:username/*rest", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"username": c.Param("username"),
-			"rest":     c.Param("rest"),
-		})
+	adminGroup := router.Group("/admin")
+
+	adminGroup.GET("/users", func(c *gin.Context) {
+		c.String(http.StatusOK, "Page to administer roles")
 	})
+
+	adminGroup.GET("/roles", func(c *gin.Context) {
+		c.String(http.StatusOK, "Page to administer policies")
+	})
+
+	adminGroup.GET("/policies", func(c *gin.Context) {
+		c.String(http.StatusOK, "Page to administer users")
+	})
+
 	log.Fatal(router.Run(":3000"))
 }

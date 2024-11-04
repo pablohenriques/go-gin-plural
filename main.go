b88ac6303b93ee325e25411cbd5cf5c967089a34
Pablo Sousa <ph.info.cont@rede.ulbra.br>
2024-11-04 08:21:02 -0300

    feat: demo retrieving request data

--- main.go
+++ main.go
@@ -4,20 +4,40 @@ import (
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
+	"strconv"
+	"time"
 )
 
 func main() {
 	router := gin.Default()
-	router.GET("/*rest", func(c *gin.Context) {
-		url := c.Request.URL.String()
-		headers := c.Request.Header
-		cookies := c.Request.Cookies()
+	router.GET("/query/*rest", func(c *gin.Context) {
+		username := c.Query("username")
+		year := c.DefaultQuery("year", strconv.Itoa(time.Now().Year()))
+		months := c.QueryArray("month")
 
-		c.IndentedJSON(http.StatusOK, gin.H{
-			"url":     url,
-			"headers": headers,
-			"cookies": cookies,
+		c.JSON(http.StatusOK, gin.H{
+			"username": username,
+			"year":     year,
+			"months":   months,
 		})
+
+	})
+
+	router.GET("/employee", func(c *gin.Context) {
+		c.File("./public/employee.html")
+	})
+
+	router.POST("/employee", func(c *gin.Context) {
+		date := c.PostForm("date")
+		amount := c.PostForm("amount")
+		username := c.DefaultPostForm("username", "adent")
+
+		c.JSON(http.StatusOK, gin.H{
+			"date":     date,
+			"amount":   amount,
+			"username": username,
+		})
+
 	})
 
 	log.Fatal(router.Run(":3000"))
